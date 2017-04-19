76f5e9b71261a1ab8a7455cad460d044e0d6fd8e
Carlos Velázquez <carlospuli10@uma.es>
2017-04-19 12:27:14 +0200

    inicio repositorio

---
+++ src/java/com/uma/diariosur/modelo/Usuario.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uma.diariosur.modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+
+
+/**
+ *
+ * @author Carmen
+ */
+@Entity
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String nick;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_nacimiento;
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "Usuario")
+    private List<Valoracion> valoraciones;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "Usuario")
+    private List<Formulario> formulario;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "Usuario")
+    private List<Megusta> megusta;
+
+    public List<Valoracion> getValoraciones() {
+        return valoraciones;
+    }
+
+    public void setValoraciones(List<Valoracion> valoraciones) {
+        this.valoraciones = valoraciones;
+    }
+
+    public List<Formulario> getFormulario() {
+        return formulario;
+    }
+
+    public void setFormulario(List<Formulario> formulario) {
+        this.formulario = formulario;
+    }
+
+    public List<Megusta> getMegusta() {
+        return megusta;
+    }
+
+    public void setMegusta(List<Megusta> megusta) {
+        this.megusta = megusta;
+    }
+    
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (nick != null ? nick.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.nick == null && other.nick != null) || (this.nick != null && !this.nick.equals(other.nick))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.diariosur1.Usuario[ nick=" + nick + " ]";
+    }
+    
+}
