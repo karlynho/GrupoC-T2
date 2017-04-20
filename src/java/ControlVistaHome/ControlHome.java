eff5c90509105dec0389785ac811d7d34d4a2b66
StevenM <smontoyav5@gmail.com>
2017-04-21 00:06:52 +0200

    Creacion de PaginaHome y Bean de Control
    Se ha creado la pagina Home que se mostrara cuando no esta regitrado el
    usuario

---
+++ src/java/ControlVistaHome/ControlHome.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControlVistaHome;
+import com.uma.diariosur.modelo.Periodista;
+import javax.inject.Named;
+import com.uma.diariosur.modelo.Usuario;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+/**
+ *
+ * @author steven
+ */
+@Named(value = "controlHome")
+@SessionScoped
+public class ControlHome implements Serializable{
+
+    private Usuario usuario;
+    private Periodista periodista;
+    
+    public void setUsuario(Usuario usuario){
+        this.usuario = usuario;
+    }
+    public Usuario getUsuario(){
+        return usuario;
+    }
+    
+    public void setPeriodista(Periodista periodista){
+        this.periodista = periodista;
+    }
+    
+    public Periodista getPeriodista(){
+        return periodista;
+    }
+    
+    public String home(){
+        if(this.usuario == null){
+            if(this.periodista==null){
+                //No hay usuario Logueado, por tanto mostramos pagina principal
+                return "PaginaHome.xhtml";
+            }else{
+                //Identificado como Periodista
+                return "PaginaHomePeriodista.xhtml";
+            }
+        }else{
+            //Identificado como Usuario Normal
+            return "PaginaHomeUsuarioNormal.xhtml";
+        }
+    }
+    
+    public String logout(){
+        //Cerramos la sesion
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        ctx.getExternalContext().invalidateSession();
+        periodista = null;
+        usuario = null;
+        return "login.xhtml";
+    }
+    
+    
+    /**
+     * Creates a new instance of ControlHome
+     */
+    public ControlHome() {
+        
+    }
+    
+}
